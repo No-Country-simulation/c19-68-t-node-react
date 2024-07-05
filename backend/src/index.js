59fa9081a860e4a7aa24317ee13add1ca34f61e1
AxelSchasemback <deluxe_shot.gamer@hotmail.com>
2024-07-05 19:54:55 -0300

    estructura backend
    base del servidor iniciada para su uso

---
+++ backend/src/index.js
@@ -0,0 +1,21 @@
+// cd a la carpeta backend y npm i para instalar las dependencias,
+// no tenemos todavia la base de datos por ende el .env de la url esta vacia
+// el archivo .env esta dentro de la carpeta config "/config/.env"
+// actualmente el .env solo contiene el PORT=8080
+
+import express from 'express';
+import mongoose from 'mongoose';
+import { MONGODB_URL, PORT } from './config/config.js';
+
+await mongoose.connect(`${MONGODB_URL}`);
+console.log(`Base de datos conectada`);
+
+export const app = express();
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
