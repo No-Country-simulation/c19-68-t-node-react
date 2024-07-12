3e4dff3486cb57c921de031f7a22a86e4520b4b3
rubenevivancos <rubenevivancos@hotmail.com>
2024-07-11 22:40:41 -0500

    Validaciones iniciales

--- backend/src/routes/patients.routes.js
+++ backend/src/routes/patients.routes.js
@@ -6,8 +6,19 @@ const router = Router();
 por eso solo se contiene mensajes */
 
 router.post("/register", (req, res)=> {
+
+  const { name, mail, pass, phone, address, birthdate, emergencyContact, healthInsurance, 
+          bloodType, allergies, currentMedications, medicalHistory } = req.body;
+
+  //Validamos los datos obligatorios
+  if (!name || !pass || !mail || !phone || !specialty || !licenseNumber || !registrationDate || !calendar) {
+    return res.status(400).send("Error: Datos requeridos no recibidos");
+  }
+
   res.status(200).json({message: "register"});
 })
+
+
 
 router.post("/login", (res, req)=> {
   const {email, password} = req
