4ee3e4950df7012f55c13306a833053a8219687f
simon.150@hotmail.com <simon.150@hotmail.com>
2024-07-16 18:09:57 -0500

    ...

--- backend/src/routes/doctor.routes.js
+++ backend/src/routes/doctor.routes.js
@@ -3,18 +3,6 @@ import controllerDoc from "../controllers/doctor.controller.js";
 
 const router = Router();
 
-router.post("/registerDoc", controllerDoc.registerDoc);
-
-router.post("/logInDoc", controllerDoc.logInDoc);
-
-router.post("/logOutDoc", controllerDoc.logOutDoc);
-
-router.post("/editProfileDoc", controllerDoc.editProfileDoc);
-
-router.get("/getDoc/:id", controllerDoc.getDoc);
-
-router.get("/profileDoc", controllerDoc.profileDoc);
-
-router.get("/getAllDoc", controllerDoc.getAllDoc);
+router.post("/doctors", controllerDoc.registerDoc);
 
 export default router;
