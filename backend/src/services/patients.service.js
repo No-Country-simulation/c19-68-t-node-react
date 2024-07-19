7ff422a6dea8905d6995e1aa0663795eb50cdcdf
rubenevivancos <rubenevivancos@hotmail.com>
2024-07-18 19:05:55 -0500

    Ordenando codigo del registro de paciente

--- backend/src/services/patients.service.js
+++ backend/src/services/patients.service.js
@@ -1,10 +1,10 @@
-import bcrypt from 'bcrypt';
 import { patientManager } from "../dao/index.dao.js";
+import { hashPassword } from "../helpers/password.helper.js";
 
 
 const servicePat = {
 
-    registerPat: async ({photo, 
+    registerPat: async (photo, 
                          firstName, 
                          lastName, 
                          gender, 
@@ -13,23 +13,29 @@ const servicePat = {
                          password, 
                          country, 
                          creditCard, 
-                         clinicalData}) => {
+                         clinicalData) => {
 
         try {
+
+            //Validamos los datos obligatorios
+            if ( !firstName || !lastName || !email || !phone || !password ) {
+                console.log(firstName + lastName + email + phone + password);
+                console.error("ERROR: Datos requeridos no recibidos");
+                throw new Error("All fields are required");
+            }
 
             // Verificar si el paciente ya existe
             const patientExists = await patientManager.findOne({ email: email });
             if (patientExists) {
                 console.error("ERROR: El paciente ya está registrado");
-                //return res.status(400).send("ERROR: El paciente ya está registrado");
-                throw new Error("El paciente ya está registrado");
+                throw new Error("Patient already exists");
             }
 
             // Hash de la contraseña
-            const hashedPassword = await bcrypt.hash(password, 10);
+            const hashedPassword = await hashPassword(password);
 
             //Grabamos el modelo Patient
-            const newPatient = {
+            const newPatient = await patientManager.create({
                 photo, 
                 firstName, 
                 lastName, 
@@ -40,71 +46,15 @@ const servicePat = {
                 country, 
                 creditCard, 
                 clinicalData
-            };
-            const patient = await patientManager.create(newPatient);
+            });
 
             console.log("Registro exitoso del paciente");
-            return patient;
+            return newPatient;
 
         } catch (error) {
-            throw error;
+            throw new Error("Error register Patient: " + error.message);
         }
     }
 }
 
 export default servicePat;
-
-
-/*
-class PatientService {
-
-    async registerPacient({
-        photo, 
-        firstName, 
-        lastName, 
-        gender, 
-        email, 
-        phone, 
-        password, 
-        country, 
-        creditCard, 
-        clinicalData
-    }) {
-
-        try {
-
-            // Verificar si el paciente ya existe
-            const patientExists = await PatientDAO.findByMail(email);
-            if (patientExists) {
-                console.error("ERROR: El paciente ya está registrado");
-                //return res.status(400).send("ERROR: El paciente ya está registrado");
-                throw new Error("El paciente ya está registrado");
-            }
-
-            // Hash de la contraseña
-            const hashedPassword = await bcrypt.hash(password, 10);
-
-            //Grabamos el modelo Patient
-            const newPatient = {
-                photo, 
-                firstName, 
-                lastName, 
-                gender, 
-                email, 
-                phone, 
-                password: hashedPassword,
-                country, 
-                creditCard, 
-                clinicalData
-            };
-            await PatientDAO.create(newPatient);
-
-            console.log("Registro exitoso del paciente");
-
-        } catch (error) {
-            throw error;
-        }
-    }
-}
-
-export default new PatientService();*/
