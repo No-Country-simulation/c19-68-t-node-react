1479a963ccf5ec5fc0a4e8b255e4799f746efd7e
unknown <josearuweb03@gmail.com>
2024-07-24 01:15:03 +0200

    Created: Onbording Mobile

---
+++ frontend/src/app/onbording/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+import BallBackground from "@/components/ui/ballBackground";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "E-medicine",
+  description: "E-medicine appointment scheduling made easy.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} relative flex flex-col-reverse lg:flex lg:flex-row`}>
+        <BallBackground/>
+        {children}
+      </body>
+    </html>
+  );
+}
