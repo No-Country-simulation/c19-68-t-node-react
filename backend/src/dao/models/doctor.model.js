81c78e234e87c3eac2ee653698d5f8c182eae4b2
guebebraga <guebe.braga5@gmail.com>
2024-07-23 17:59:20 -0300

    some changes

--- backend/src/dao/models/doctor.model.js
+++ backend/src/dao/models/doctor.model.js
@@ -29,7 +29,7 @@ const doctorSchema = new mongoose.Schema(
     professionalCertificates: [
       {
         type: String,
-        required: true,
+        required: false,
         trim: true,
       },
     ],
