2f8a2c01ed00fcfc1b57c2b162dcb24650c83c73
simon.150@hotmail.com <simon.150@hotmail.com>
2024-07-27 13:41:14 -0500

    environment variable path change

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,12 +3,9 @@ import connectDB from "./config/db.js";
 import dotenv from "dotenv";
 
 //config environment variables
-dotenv.config({
-    path: './src/config/.env'
-});
+dotenv.config();
 
 //alternative port
-//const PORT = process.env.PORT || 6000;
 const PORT = process.env.PORT || 8080;
 
 //connect to database from config/db.js
