e1c884c6da85f5e49ab81b29cef95fc7cc5dbb2b
Ulises <ulisescappa@gmail.com>
2024-07-29 16:55:23 -0300

    Add cors app.js file.

--- backend/src/config/socketConfig.js
+++ backend/src/config/socketConfig.js
@@ -1,20 +1,45 @@
-import http from 'http';
-import { Server } from 'socket.io';
+import http from "http";
+import { Server } from "socket.io";
 
 export const initializeServer = (app) => {
   const server = http.createServer(app);
   const io = new Server(server);
 
-  io.on('connection', (socket) => {
-    console.log('New client connected');
-    
-    socket.on('disconnect', () => {
-      console.log('Client disconnected');
+  io.on("connection", (socket) => {
+    console.log("New client connected");
+
+    socket.on("disconnect", () => {
+      console.log("Client disconnected");
     });
 
     // Define other socket events here
   });
 
   const PORTSOCKET = process.env.PORTSOCKET || 3001;
-  server.listen(PORTSOCKET, () => console.log(`Socket.io running on port ${PORTSOCKET}`));
+  server.listen(PORTSOCKET, () =>
+    console.log(`Socket.io running on port ${PORTSOCKET}`)
+  );
 };
+
+// import { Server } from "socket.io";
+
+// export function initializeServer(app) {
+//   const httpServer = app.listen(3001, () => {
+//     console.log("Server running on port 3001");
+//   });
+
+//   const io = new Server(httpServer, {
+//     cors: {
+//       origin: "*",
+//       methods: ["GET", "POST"],
+//     },
+//   });
+
+//   io.on("connection", (socket) => {
+//     console.log("A user connected");
+
+//     socket.on("disconnect", () => {
+//       console.log("A user disconnected");
+//     });
+//   });
+// }
