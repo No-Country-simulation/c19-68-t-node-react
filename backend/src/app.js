56810c696791b17652be550f7bd3fb92f63ba502
simon.150@hotmail.com <simon.150@hotmail.com>
2024-08-01 18:30:34 -0500

    "Added cors dependency and imported it in app.js"

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import morgan from "morgan";
 import paymentGatewayRoutes from "./routes/paymentGateway.routes.js";
 import appointmentsRoutes from "./routes/appointments.routes.js";
