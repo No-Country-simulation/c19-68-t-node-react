af3be80b7b07a0077eaebd622bd870801f38738d
rubenevivancos <rubenevivancos@hotmail.com>
2024-08-01 04:41:55 -0500

    page for successful payment

---
+++ frontend/src/app/successfulPayment/page.js
@@ -0,0 +1,11 @@
+import SuccessfulPaymentForm from "@/components/successfulPaymet";
+
+const Success = () => {
+  return (
+    <div className="w-screen flex justify-center overflow-hidden">
+      <SuccessfulPaymentForm />
+    </div>
+  );
+};
+
+export default Success;
---
+++ frontend/src/components/successfulPaymet.js
@@ -0,0 +1,57 @@
+import React from 'react';
+
+const SuccessfulPaymentForm = () => {
+  return (
+    <div className="login-container overflow-hidden w-[80%] h-full flex flex-col items-center bg-white px-4">
+      {/* Contenedor del formulario */}
+      <div className="login-form w-full max-w-[400px] bg-white rounded-lg shadow-lg p-6">
+        <form className="flex flex-col gap-4">
+
+          {/* Mensaje de pago exitoso */}
+          <div className="text-center text-xl font-bold text-green-600 mb-8">
+            ¡Pago completado con éxito!
+          </div>
+
+          {/* Detalles del pago */}
+          <div className="text-gray-700 mb-4">
+            <p className="font-bold mb-5">Detalles del pago:</p>
+            <div className="mb-4">
+              <table className="min-w-full">
+                <tbody>
+                  <tr>
+                    <td className="font-bold p-2">Número de pedido:</td>
+                    <td className="p-2">#12345</td>
+                  </tr>
+                  <tr>
+                    <td className="font-bold p-2">Total pagado:</td>
+                    <td className="p-2">$50.00 USD</td>
+                  </tr>
+                  <tr>
+                    <td className="font-bold p-2">Producto:</td>
+                    <td className="p-2">Suscripción mensual</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+
+          {/* Agradecimiento y contacto */}
+          <div className="text-gray-700 text-center">
+            <p>Gracias por tu compra. Si tienes alguna pregunta, no dudes en contactarnos a través de nuestro servicio de atención al cliente.</p>
+          </div>
+
+          {/* Botón de redirección */}
+          <button
+            type="submit"
+            className="bg-[#812B75] text-white py-2 rounded-lg hover:bg-gray-500 transition w-full mt-8"
+          >
+            Ir a la página principal
+          </button>
+
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default SuccessfulPaymentForm;
