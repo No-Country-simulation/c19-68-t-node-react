a2a5bcd204f9feaf30cdc043f78707c7b5995c8f
Ulises <ulisescappa@gmail.com>
2024-08-02 00:47:15 -0300

    Fix stripe key bug.

--- backend/src/services/paymentGateway.service.js
+++ backend/src/services/paymentGateway.service.js
@@ -1,11 +1,31 @@
-import Stripe from 'stripe';
+import Stripe from "stripe";
 import CustomError from "../middlewares/error.middleware.js";
+import dotenv from "dotenv";
+dotenv.config();
 
 const stripe = new Stripe(`${process.env.STRIPE_SECRET_KEY}`);
 
 class PaymentGatewayService {
-  async createSession({ name, description, currency, unit_amount, quantity, mode, success_url, cancel_url }) {
-    if (!name || !description || !currency || !unit_amount || !quantity || !mode || !success_url || !cancel_url) {
+  async createSession({
+    name,
+    description,
+    currency,
+    unit_amount,
+    quantity,
+    mode,
+    success_url,
+    cancel_url,
+  }) {
+    if (
+      !name ||
+      !description ||
+      !currency ||
+      !unit_amount ||
+      !quantity ||
+      !mode ||
+      !success_url ||
+      !cancel_url
+    ) {
       throw new CustomError("Falta enviar datos obligatorios", 422);
     }
 
@@ -16,22 +36,25 @@ class PaymentGatewayService {
             price_data: {
               product_data: {
                 name: name,
-                description: description
+                description: description,
               },
               currency: currency,
-              unit_amount: unit_amount // Se pone en centimos, 20000 equivale a 200.00 dolares
+              unit_amount: unit_amount, // Se pone en centimos, 20000 equivale a 200.00 dolares
             },
-            quantity: quantity
-          }
+            quantity: quantity,
+          },
         ],
         mode: mode,
         success_url: success_url,
-        cancel_url: cancel_url
+        cancel_url: cancel_url,
       });
 
       return session;
     } catch (error) {
-      throw new CustomError("Error creating Stripe session: " + error.message, 500);
+      throw new CustomError(
+        "Error creating Stripe session: " + error.message,
+        500
+      );
     }
   }
 
@@ -40,7 +63,10 @@ class PaymentGatewayService {
       const session = await stripe.checkout.sessions.retrieve(sessionID);
       return session;
     } catch (error) {
-      throw new CustomError("Error retrieving Stripe session: " + error.message, 500);
+      throw new CustomError(
+        "Error retrieving Stripe session: " + error.message,
+        500
+      );
     }
   }
 }
