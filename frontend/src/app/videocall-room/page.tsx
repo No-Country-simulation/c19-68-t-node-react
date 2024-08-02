0192d9c03ecd52bdb66b91e24f331cc3f965893f
guebebraga <guebe.braga5@gmail.com>
2024-08-02 14:16:35 -0300

    se agrego componente videocall

---
+++ frontend/src/app/videocall-room/page.tsx
@@ -0,0 +1,15 @@
+import VideoCall from './VideoCall';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Videollamada</h1>
+      <VideoCall />
+    </div>
+  );
+}
+
+//dependencias para instalar
+
+//npm install express socket.io mongoose
+//npm install socket.io-client peerjs
