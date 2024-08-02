39740c9ad84115effc261f938b6615046c060c63
guebebraga <guebe.braga5@gmail.com>
2024-08-02 11:20:36 -0300

    subo antes de rompmer todo

--- backend/src/dao/videoCallDAO.js
+++ backend/src/dao/videoCallDAO.js
@@ -1,78 +1,11 @@
-/*import VideoCall from "./models/VideoCall.js";
 
-class VideoCallDAO {
-  static async createCall(roomId) {
-    const videoCall = new VideoCall({
-      roomId,
-      participants: [],
-      startTime: new Date(),
-    });
-    return await videoCall.save();
-  }
-
-  static async updateCall(roomId, participant) {
-    return await VideoCall.findOneAndUpdate(
-      { roomId },
-      { $addToSet: { participants: participant } },
-      { new: true }
-    );
-  }
-
-  static async endCall(roomId) {
-    return await VideoCall.findOneAndUpdate(
-      { roomId },
-      { endTime: new Date() },
-      { new: true }
-    );
-  }
-}
-
-export default VideoCallDAO;
-*/
-/*
-import VideoCall from "./models/VideoCall.js";
-
-class VideoCallDAO {
-  static async createCall(roomId) {
-    const videoCall = new VideoCall({
-      roomId,
-      participants: [],
-      startTime: new Date(),
-    });
-    return await videoCall.save();
-  }
-
-  static async updateCall(roomId, participant) {
-    return await VideoCall.findOneAndUpdate(
-      { roomId },
-      { $addToSet: { participants: participant } },
-      { new: true }
-    );
-  }
-
-  static async endCall(roomId) {
-    return await VideoCall.findOneAndUpdate(
-      { roomId },
-      { endTime: new Date() },
-      { new: true }
-    );
-  }
-
-  // Nuevo método para obtener información de la llamada
-  static async getCallInfo(roomId) {
-    return await VideoCall.findOne({ roomId });
-  }
-}
-
-export default VideoCallDAO;
-*/
 import VideoCall from "./models/VideoCall.js"
 
 class VideoCallDAO {
-  static async createCall(roomId) {
-    const videoCall = new VideoCall({
+  static async createCall(roomId) { 
+      const videoCall = new VideoCall({
       roomId,
-      participants: [],
+      participants:[], 
     });
     return await videoCall.save();
   }
--- backend/src/services/videoCallService.js
+++ backend/src/services/videoCallService.js
@@ -1,29 +1,10 @@
-/*
-import VideoCallDAO from '../dao/videoCallDAO.js';
-
-class VideoCallService {
-  static async initiateCall(roomId) {
-    return await VideoCallDAO.createCall(roomId);
-  }
-
-  static async joinCall(roomId, participant) {
-    return await VideoCallDAO.updateCall(roomId, participant);
-  }
-
-  static async endCall(roomId) {
-    return await VideoCallDAO.endCall(roomId);
-  }
-}
-
-export default VideoCallService;
-*/
 
 import VideoCallDAO from '../dao/videoCallDAO.js';
 
 
 class VideoCallService {
-  static async initiateCall(roomId) {
-    return await VideoCallDAO.createCall(roomId);
+  static async initiateCall(roomId) { 
+    return await VideoCallDAO.createCall(roomId ); 
   }
 
   static async joinCall(roomId, participant) {
