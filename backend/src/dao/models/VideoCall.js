bed2f6cf5eec50bef9f3830edbca16dae1f45e8d
guebebraga <guebe.braga5@gmail.com>
2024-08-07 21:50:31 -0300

    se borro el codigo comentado

--- backend/src/dao/models/VideoCall.js
+++ backend/src/dao/models/VideoCall.js
@@ -1,18 +1,3 @@
-/*import mongoose from 'mongoose';
-
-const VideoCallSchema = new mongoose.Schema({
-  roomId: String,
-  participants: [String],
-  startTime: Date,
-  endTime: Date
-});
-
-
-const VideoCall = mongoose.model('VideoCall', VideoCallSchema);
-
-export default VideoCall;
-*/
-
 import mongoose from 'mongoose';
 
 const VideoCallSchema = new mongoose.Schema({
